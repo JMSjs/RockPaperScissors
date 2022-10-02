4f5b2c8a7329bd2bbd76494d158a909ac904fab3
jmsjs <jmsong8@gmail.com>
2022-10-02 14:26:33 -0700

    removed console.logs => added DOM ui

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 let roundWins = 0;
+const displayRoundWins = document.querySelector('#roundWins');
 let roundLosses = 0;
-// let rock = document.querySelector('#rock');
-// let paper = document.querySelector('#paper');
-// let scissors = document.querySelector('#scissors');
-let buttons = document.querySelectorAll('.buttons');
+const displayRoundLosses = document.querySelector('#roundLosses');
+let totalRounds = 5;
+const displayTotalRounds = document.querySelector('#numRounds')
+let currRound = 1;
+const displayCurrRound = document.querySelector('#currRound');
+const textLog = document.querySelector('.textLog');
+const buttons = document.querySelectorAll('.buttons');
+const btnIncrementRound = document.querySelector('#incrementRound');
+const btnDecrementRound = document.querySelector('#decrementRound');
+
 
 // this function creates a random hand for the computer
 function getComputerChoice() {
@@ -21,58 +28,63 @@ function getComputerChoice() {
 }
 
 // this function executes 1 complete round of R.P.S.
-function playRound() {
-    buttons.forEach(button => button.addEventListener('click' , function(e) {
-        let playerSelection = e.target.id;
-        let computerSelection = getComputerChoice();
-        evaluateRound(playerSelection, computerSelection);
-    }));
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    displayRound();
+    displayRoundWinner(evaluateRound(playerSelection, computerSelection));
+    currRound++;
 }
 
-// this function executes an entire game
-function game() {
-    for (i = 1; i <= 5; i++) {
-        console.log(" ");
-        console.log(`******* Round [${i}] out of [5] *******`)
-        console.log(playRound());
-    }
-    whoWon();
+function displayRound() {
+    let currentRoundText = document.createElement('p');
+    currentRoundText.textContent = `******* Round [${currRound}] out of [${totalRounds}] *******`;
+    textLog.appendChild(currentRoundText);
+    displayCurrRound.textContent = currRound;
+}0
+
+function displayRoundWinner(func) {
+    let roundWinText = document.createElement('p');
+    roundWinText.textContent = func;
+    textLog.appendChild(roundWinText);
+
 }
 
 // this function executes when there is a tie game and returns a string
 function tieGame(yourString) {
-    return `     Tie. Both players chose [${yourString}].`;
+    return `Tie. Both players chose [${yourString}].`;
 }
 
-// this funciton executes if player wins and returns a string
+// this function executes if player wins and returns a string
 function playerWin(yourString, cpuString){
     roundWins++;
-    return `     ROUND WON! [${yourString}] beats [${cpuString}]!`;
+    displayRoundWins.textContent = roundWins;
+    return `ROUND WON! [${yourString}] beats [${cpuString}]!`;
 }
 
 //this function executes if the computer wins and returns a string
 function playerLose(yourString, cpuString){
     roundLosses++;
-    return `     Round lost. [${yourString}] loses to [${cpuString}].`;
+    displayRoundLosses.textContent = roundLosses;
+    return `Round lost. [${yourString}] loses to [${cpuString}].`;
 }
 
+// game winner display message
+function displayWinner(func) {
+    const winnerText = document.createElement('h3');
+    winnerText.textContent = func;
+    winnerText.style.backgroundColor = 'yellow';
+    textLog.appendChild(winnerText);
+}
 //this function executes to determine and declare the final winner after a game is finished
 function whoWon() {
     if (roundWins === roundLosses) {
-        console.log("");
-        console.log("========================================");
-        console.log("FINAL RESULT....game was a tie");
-        console.log("========================================");
+        return 'FINAL RESULT....game was a tie';
     } else if (roundWins > roundLosses) {
-        console.log("");
-        console.log("========================================");
-        console.log(`FINAL RESULT....YOU WIN [${roundWins}] to [${roundLosses}]!!`);
-        console.log("========================================");
+        displayRoundWins.style.backgroundColor = 'yellow';
+        return `FINAL RESULT....YOU WIN [${roundWins}] to [${roundLosses}]!!`;
     } else {
-        console.log("");
-        console.log("========================================");
-        console.log(`FINAL RESULT....You lost [${roundWins}] to [${roundLosses}].`);
-        console.log("========================================");
+        displayRoundLosses.style.backgroundColor = 'yellow';
+        return `FINAL RESULT....You lost [${roundWins}] to [${roundLosses}].`;
     }
 }
 
@@ -104,4 +116,40 @@ function evaluateRound(yourString, cpuString) {
     }
 }
 
-game();
+btnIncrementRound.addEventListener('click', function(e) {
+    totalRounds++;
+    displayTotalRounds.textContent = totalRounds;
+    currRound = 1;
+    displayCurrRound.textContent = currRound;
+    roundWins = 0;
+    displayRoundWins.textContent = roundWins;
+    displayRoundWins.style.backgroundColor = '';
+    roundLosses = 0;
+    displayRoundLosses.textContent = roundLosses;
+    displayRoundLosses.style.backgroundColor = '';
+    textLog.textContent = "text log:";
+})
+
+btnDecrementRound.addEventListener('click', function(e) {
+    totalRounds--;
+    if (totalRounds < 1) {totalRounds = 1};
+    displayTotalRounds.textContent = totalRounds;
+    currRound = 1;
+    displayCurrRound.textContent = currRound;
+    roundWins = 0;
+    displayRoundWins.textContent = roundWins;
+    displayRoundWins.style.backgroundColor = '';
+    roundLosses = 0;
+    displayRoundLosses.textContent = roundLosses;
+    displayRoundLosses.style.backgroundColor = '';
+    textLog.textContent = "text log:";
+})
+
+buttons.forEach(button => button.addEventListener('click' , function(e) {
+    if (currRound > totalRounds) {
+        displayWinner(whoWon());
+        return;
+    }
+    let playerSelection = e.target.id;
+    playRound(playerSelection);
+}));
